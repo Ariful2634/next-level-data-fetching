0e6fd316ab29a7bcadd57c4a6824024c5b3c852f
Ariful2634 <ariful2634@gmail.com>
2023-12-09 19:17:43 +0600

    exploring api

---
+++ src/app/api/blogs/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server"
+
+// GET Method
+export const GET = ()=>{
+   return NextResponse.json({message: "Hello, World"})
+}
+
+
+// POST Method
+export const POST = ()=>{}
+
+
+// PATCH Method
+export const PATCH = ()=>{}
+
+
+// PUT Method
+export const PUT = ()=>{}
+
+//  DELETE Method
+export const DELETE = ()=>{}
